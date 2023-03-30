917cfd5f42cd5973c1cd676efe8bdea36eef1e1a
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-03-30 10:02:05 +0200

    Milestone 1

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// Models 
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
